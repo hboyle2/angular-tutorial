43e403c09135fa707f215b4bbd2427665676f99b
Haley Boyle <haleyjboyle@gmail.com>
2017-12-05 15:53:11 -0700

    added message functionality

--- routes/app.js
+++ routes/app.js
@@ -5,28 +5,8 @@ var User = require('../models/user')
 
 
 router.get('/', function (req, res, next) {
-    User.findOne({}, function(err, doc){
-        if (err){
-            return res.send('error!')
-        }
-
-        res.render('node', {email:doc.email});
-    })
+   res.render('index');
+  
 });
-
-
-
-router.post('/', function(req, res, next) {
-    var email = req.body.email;
-    var user = new User({
-        firstName: 'Haley',
-        lastName: 'Boyle',
-        password: 'bob',
-        email:  email
-    });
-    user.save();
-    
-    res.redirect('/');
-})
 
 module.exports = router;
